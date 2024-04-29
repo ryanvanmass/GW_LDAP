8b9d32e3840cced69c3772bc03ce74e228c2ae8c
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2024-04-29 19:36:32 -0400

    Initial Commit

---
+++ connect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install sssd -y
+
+
+sudo cp $pwd/sssd.conf /etc/sssd/
+
+sudo chown root:root /etc/sssd/sssd.conf
+sudo chmod 600 /etc/sssd/sssd.conf
+
+sudo systemctl restart sssd
