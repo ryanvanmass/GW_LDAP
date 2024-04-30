f3ca3bb3416e6efe886c48cab85dcaba9b0d9523
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2024-04-29 21:02:45 -0400

    Add Command to create Home directory

--- connect.sh
+++ connect.sh
@@ -10,5 +10,7 @@ sudo chmod 600 /etc/sssd/sssd.conf
 
 sudo systemctl restart sssd
 
+sudo su -c 'echo "session optional  pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/common-session'
+
 sudo usermod -aG sudo ryan
 su ryan -c "ssh-import-id-gh ryanvanmass"
