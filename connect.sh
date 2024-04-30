31e2162e75fbc158c664a2ba850b1ff4db581be9
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2024-04-29 21:03:54 -0400

    Comment Code

--- connect.sh
+++ connect.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+# Install Required Packages
 sudo apt update
 sudo apt install sssd -y
 
-
+# Copy Template Config and set permissions
 sudo cp ./sssd.conf /etc/sssd/
-
 sudo chown root:root /etc/sssd/sssd.conf
 sudo chmod 600 /etc/sssd/sssd.conf
 
+# Restart Service
 sudo systemctl restart sssd
 
+# Set option to create Home Dir's for LDAP Users
 sudo su -c 'echo "session optional  pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/common-session'
 
+
+# User Specific Config
 sudo usermod -aG sudo ryan
 su ryan -c "ssh-import-id-gh ryanvanmass"
